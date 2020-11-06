ce9c9be02b4d8eed88d903ee918ae5d6b6505886
Tereshchenko Vlad <ddiavall@yandex.ru>
2020-11-06 22:32:01 +0300

    Added key structures

--- Lab8/Lab8/Lab8/Form1.cs
+++ Lab8/Lab8/Lab8/Form1.cs
@@ -10,8 +10,15 @@ using System.Windows.Forms;
 
 namespace Lab8
 {
+    public enum Axis { AXIS_X, AXIS_Y, AXIS_Z, LINE };
+    public enum Projection { PERSPECTIVE = 0, ISOMETRIC, ORTHOGR_X, ORTHOGR_Y, ORTHOGR_Z };
+
     public partial class Form1 : Form
     {
+        Graphics g;
+        Projection projection = 0;
+        Polyhedron figure = null;
+
         public Form1()
         {
             InitializeComponent();
